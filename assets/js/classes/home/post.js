bacae3ab09c2ad1958d62c8f46b4178631032c75
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2024-03-31 22:39:56 +0200

    Update post.js

--- assets/js/classes/home/post.js
+++ assets/js/classes/home/post.js
@@ -1,28 +1,26 @@
 
 //ATTACHED PREVIEW TO FILES
-{/* <div class="attached-preview-list">
-            <div class="attached-preview">
-                <div class="attached-preview-image">
-                    <img src="" alt="">
-                </div>
-                <div class="attached-preview-text">
-                </div>
-            </div>
-        </div> */}
+{/*
+<div class="attached-preview-list">
+    <div class="attached-preview">
+        <div class="attached-preview-image">
+            <img src="" alt="">
+        </div>
+        <div class="attached-preview-text">
+        </div>
+    </div>
+</div>
+*/}
 
 
 //COMMENTS
-{/* <div class="comment-section">
-
-
-<div class="comment-list">
-    <div class="comment">
-
+{/* 
+<div class="comment-section">
+    <div class="comment-list">
+        <div class="comment"></div>
     </div>
-
 </div>
-
-</div> */}
+*/}
 
 
 export class Post {
@@ -46,12 +44,16 @@ export class Post {
             var newPost = this.postHTML;
             if (textareaPost.value != "") {
                 newPost = newPost.replace('[CONTENIDO]' , textareaPost.value);
-                this.postList.push(newPost);
-                postListDiv.innerHTML = "";
-                this.postList.forEach( post =>{
-                    postListDiv.innerHTML += post;
-                })
-                
+
+                // LLAMADA A BASE DE DATOS, 
+                this.BBDDcallSendPost();
+
+
+                // this.postList.push(newPost);
+                // postListDiv.innerHTML = "";
+                // this.postList.forEach( post =>{
+                //     postListDiv.innerHTML += post;
+                // });
             }
             textareaPost.value = "";
         })
@@ -63,5 +65,36 @@ export class Post {
                 document.getElementById('current').innerHTML = 300;
             }
         })
+    }
+
+    crea_query_string() {
+        var obj = {"codAula": this.classCode , "email": localStorage.getItem("email")};
+        var cadena = JSON.stringify(obj);
+        return cadena;
+    }
+
+    BBDDcallSendPost(){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange=function() {
+            if(this.readyState==4 && this.status==200) {
+                console.log(this.responseText);
+                var datos = JSON.parse(this.responseText);
+                if (datos == "") {
+                    //document.getElementById('datos').innerHTML = "La contraseña o el usuario introducidos son incorrectos";
+                    console.log("Fallo");
+                }else{
+                    console.log(datos);
+                    console.log("Completado");
+                    localStorage.setItem("lastCodAula", this.classCode);
+                    enviarRuta('/');
+                }
+
+            }
+        };
+        //PAGINA ENVIO PHP
+        xmlhttp.open('POST','assets/php/posts/classCode.php');
+        xmlhttp.setRequestHeader('Content-Type','application/json;charset=UTF-8');
+        let cadena = this.crea_query_string();
+        xmlhttp.send(cadena);
     }
 }
